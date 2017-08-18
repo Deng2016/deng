a9f81105edcb1c0e5b541d95961f7740ee5530ea
邓清涌 <yu12377@163.com>
2017-08-18 22:25:44 +0800

    first commit

---
+++ build/lib/deng/encrypt.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*-
+import sys
+reload( sys )
+sys.setdefaultencoding('utf-8')
+import json
+from urllib import urlencode
+import requests
+
+class encrypt(object):
+    # 计算MD5值
+    def getmd5(self, paramStr):
+        try:
+            import hashlib
+            hash = hashlib.md5()
+        except ImportError:
+            # for Python << 2.5
+            import md5
+            hash = md5.new()
+        hash.update(paramStr)
+        return  hash.hexdigest()
+
+    # 参数升序排序及拼接
+    def join_array(self,param):
+        str_data='' 
+        sorted_x = sorted(param.iteritems(), key=lambda param : param[0])
+        for tuple in sorted_x:
+            str_data+=str(tuple[0])+str(tuple[1])
+        return str_data
