18d03aa533f83b7def5d817c3c9905ead015dc69
邓清涌 <yu12377@163.com>
2017-08-22 17:01:47 +0800

    增加自动部署功能

---
+++ fabfile.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding:utf-8
+from fabric.api import env, settings, cd, run, prefix
+from deng.colors import *
+
+servers = [
+            {"host": "ci", "virtualenv": "cloud_ci"},
+            {"host": "ejen-root", "virtualenv": ""}
+           ]
+
+
+def update():
+    """将新push的脚本更新到jenkins上"""
+    for server in servers:
+        print(blue("\n\n开始部署: %s, %s" % (server["host"], server["virtualenv"])))
+        with settings(forward_agend=True, use_ssh_config=True, host_string=server["host"], colorize_errors=True):
+            print(blue("部署：下载源码"))
+            run("rm -rf ~/deng")
+            run("git clone ssh://git@gitlab/ci-python/deng.git")
+            with cd("~/deng"):
+                if len(server["virtualenv"]) > 0:
+                    with prefix("workon %s" % server["virtualenv"]):
+                        try:
+                            print(blue("部署：删除旧版"))
+                            run("pip uninstall -y deng")
+                        except:
+                            pass
+                        print(blue("部署：部署新版"))
+                        run("python setup.py install")
+                else:
+                    try:
+                        print(blue("部署：删除旧版"))
+                        run("pip uninstall -y deng")
+                    except:
+                        pass
+                    print(blue("部署：部署新版"))
+                    run("python setup.py install")
+            print(blue("部署：删除源码"))
+            run("rm -rf ~/deng")
+        print(blue("完成部署: %s, %s" % (server["host"], server["virtualenv"])))
--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ from setuptools import setup, find_packages
 setup(
     name="deng",
     version="0.1",
+    summary="个人常用方法封装",
+    author="dengqingyong",
+    author_email="yu12377@163.com",
     packages=find_packages(),
     exclude_package_data={"": [".gitignore"]},
     install_requires=[
