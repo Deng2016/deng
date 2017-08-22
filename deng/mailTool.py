077b8b67d1386378f9c7b5acb847425b3fb02895
邓清涌 <yu12377@163.com>
2017-08-22 15:38:18 +0800

    1.添加自动部署功能； 2.添加colors.py;

--- deng/mailTool.py
+++ deng/mailTool.py
@@ -9,7 +9,9 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from os.path import exists
+from email.utils import formatdate
+from os.path import exists, basename
+from deng.colors import *
 
 
 class MailTool(object):
@@ -35,12 +37,16 @@ class MailTool(object):
         self.send_username = send_username
         self.send_password = send_password
 
-    def _attachment(self, attobj, filename):
+    def _add_attach(self, attobj, filename):
         """将单个文件添加到附件对象中"""
         if exists(filename):
-            attachname = filename.split('/')[-1]
-            if len(filename) == 0:
-                attachname = filename.split('\\')[-1]
+            attachname = basename(filename)
+            # attachname = Header(attachname, "utf-8")
+            # # linux系统
+            # attachname = filename.split('/')[-1]
+            # # windows系统
+            # if len(filename) == 0:
+            #     attachname = filename.split('\\')[-1]
         else:
             raise IOError("文件不存: %s" % filename)
         attach = MIMEText(open(filename, "rb").read(), "base64", "utf-8")
@@ -51,63 +57,60 @@ class MailTool(object):
 
     def _attachment(self, msg, attachs):
         """处理附件,将附件添加到邮件中"""
-        msg_part = MIMEMultipart()
-        msg_part["From"] = msg["From"]
-        msg_part["To"] = msg["To"]
-        msg_part["Subject"] = msg["Subject"]
-        # print msg.get_payload(decode=True)
-        msg_part.attach(msg)
         if isinstance(attachs, (list, tuple)):
             for attach in attachs:
                 if exists(attach):
-                    msg_part = self._attachment(msg_part, attach)
+                    msg = self._add_attach(msg, attach)
                 else:
                     print "附件不存在, 跳过: %s" % attach
         else:
             if exists(attachs):
-                msg_part = self._attachment(msg_part, attachs)
+                msg = self._add_attach(msg, attachs)
             else:
                 print "附件不存在, 跳过: %s" % attachs
-                return msg
-        return msg_part
+        return msg
 
-    def send(self, receiver, subject, content, mail_format="plain", mail_charset="utf-8", attachs=None, echo=False):
+    def send(self, receiver, subject, content, mail_charset="utf-8", attachs=None, echo=False):
         """
             发送邮件
             参数:
                 receiver:接收人邮箱列表,
                 subject:邮件主题,
                 content:邮件主题,
-                mail_format:邮件格式,取值有plain和html,
                 mail_charset:邮件字符集,
                 attachs:附件路径列表
         """
-        # 转换正文类型为规范类型
-        if mail_format == "text":
-            mail_format = "plain"
+        # 字符串单收件人时转换成列表格式
+        if isinstance(receiver, (str, unicode)):
+            receiver = [receiver]
+        # 收件人格式检查
+        if not isinstance(receiver, (tuple, list)):
+            print red("参数receiver格式不正确!")
+            print red("receiver预期格式：str、tuple或list")
+            print red("receiver实际格式：%s" % type(receiver))
+        # 接收人receiver去重
+        if isinstance(receiver, (tuple, list)):
+            receiver = list(set(receiver))
 
-        # 处理邮件正文格式
-        if mail_format == "plain":
-            pass
-        elif mail_format == "html":
-            content = "<html><body>%s</body></html>" % content
-        else:
-            print "邮件格式输入错误: %s, 应该为text或html" % mail_format
-            return 1
+        # 邮件长度检查，超大时只截取前5000个字符
         if len(content) > 5000:
-            print "邮件长度：%d" % len(content)
-            print "请注意邮件内容过多，可能会被过滤掉！"
-            print "自动截取前5000个字符，后面的忽略……"
+            print yellow("邮件长度：%d" % len(content))
+            print yellow("请注意邮件内容过多，可能会被过滤掉！")
+            print yellow("自动截取前5000个字符，后面的忽略……")
             content = content[:5000]
-        msg = MIMEText(content, mail_format, mail_charset)
+
+        # msg = MIMEMultipart("alternative")
+        msg = MIMEMultipart("related")
+        # text
+        text = MIMEText(content, 'plain', mail_charset)
+        msg.attach(text)
+        # html
+        html = MIMEText("<html><body><p>%s</p></body></html>" % content, "html", mail_charset)
+        msg.attach(html)
+        msg['Date'] = formatdate(localtime=True)
         msg["Subject"] = Header(subject, mail_charset)
         msg["From"] = Header(self.send_username, mail_charset)
-        # 接收人receiver去重
-        receiver = list(set(receiver))
-        if isinstance(receiver, (list, tuple)):
-            msg["To"] = Header(', '.join(receiver), 'utf-8')
-        else:
-            msg["To"] = Header(receiver, mail_charset)
+        msg["To"] = Header(', '.join(receiver), 'utf-8')
 
         # 处理附件
         if attachs:
