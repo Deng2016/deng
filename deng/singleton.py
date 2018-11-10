3d1f7531c2c985fd1098d30df78aaa93ce4ee26d
邓清涌 <dengqingyong@daojia-inc.com>
2018-11-10 16:26:25 +0800

    增加单例模式父类

--- deng/singleton.py
+++ deng/singleton.py
@@ -10,11 +10,11 @@
 class Singleton(object):
     """单例模式父类，继承此类可快速定义单例类"""
 
-    # 已经实例化的对象类列表
+    # 已经实例化的对象类
     _instance = None
 
     # 单例模式：确保一个类只能实例化一次
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = object.__new__(cls)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
